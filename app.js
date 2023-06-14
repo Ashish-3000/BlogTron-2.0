bc53aac0043b288c61a6c214836e549b2831e74e
Ashish Singh <71404994+Ashish-3000@users.noreply.github.com>
2023-06-14 12:42:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
+require("dotenv").config();
+const mql = require("@microlink/mql");
+const bodyParser = require("body-parser");
+const app = express();
+const cors = require("cors");
+
+// MyRoutes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const tagsRoutes = require("./routes/tags");
+const blogRoutes = require("./routes/blog");
+
+// DB connection
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch(() => {
+    console.log("DB OOPs");
+  });
 
 // Constants
 const PORT = 8080;
