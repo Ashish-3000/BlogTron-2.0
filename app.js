9e1863c0b6fb76b343cbd2488e7499213fc42447
Ashish Singh <71404994+Ashish-3000@users.noreply.github.com>
2023-06-14 12:12:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-import { connect } from "mongoose";
-import express from "express";
+const mongoose = require("mongoose");
+const express = require("express");
 require("dotenv").config();
-import mql from "@microlink/mql";
-import { json } from "body-parser";
+const mql = require("@microlink/mql");
+const bodyParser = require("body-parser");
 const app = express();
-import cors from "cors";
+const cors = require("cors");
 
 // MyRoutes
-import authRoutes from "./routes/auth";
-import userRoutes from "./routes/user";
-import tagsRoutes from "./routes/tags";
-import blogRoutes from "./routes/blog";
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const tagsRoutes = require("./routes/tags");
+const blogRoutes = require("./routes/blog");
 
 // DB connection
-connect(process.env.DATABASE)
+mongoose
+  .connect(process.env.DATABASE)
   .then(() => {
     console.log("DB Connected");
   })
@@ -22,10 +23,10 @@ connect(process.env.DATABASE)
   });
 
 // Middlewares
-app.use(json());
+app.use(bodyParser.json());
 app.use(cors());
 
-// // My Routes
+// My Routes
 // app.use(authRoutes);
 // app.use(userRoutes);
 // app.use(tagsRoutes);
